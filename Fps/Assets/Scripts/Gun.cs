93c5467b33680b28e126d6dc4f6eef692bb7bde6
Tymek <almacen354@gmail.com>
2020-04-11 11:19:12 +0200

    fps

---
+++ Fps/Assets/Scripts/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+
+    public Rigidbody Bullet;
+    public Transform firePlace;
+    public float power;
+    private Rigidbody projectile;
+    private bool fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            projectile = Instantiate(Bullet, firePlace.position, transform.rotation);
+            fire = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if(fire)
+        {
+            projectile.velocity = transform.TransformDirection((Vector3.forward*-1) * power);
+            fire = false;
+            Destroy(projectile.gameObject, 3);
+
+        }
+    }
+}
---
+++ Fps/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController charContr;
+
+    private float xDir;
+    private float zDir;
+    private float yDir;
+
+    private Vector3 moveDirection;
+
+    public float speed = 10f;
+    private bool isGrounded;
+    public Transform grandCheck;
+    public LayerMask groundLayer;
+    public float groundDist = 0.2f;
+    public float jump = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(grandCheck.position, groundDist, groundLayer);
+
+        if(isGrounded && yDir <0)
+        {
+            yDir = -2f;
+        }
+
+        xDir = Input.GetAxis("Horizontal");
+        zDir = Input.GetAxis("Vertical");
+        yDir += Physics.gravity.y*Time.deltaTime;
+
+        moveDirection = transform.right * xDir * speed + transform.forward * zDir * speed + transform.up * yDir;
+
+        charContr.Move(moveDirection * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump")&&isGrounded)
+        {
+            yDir = Mathf.Sqrt(jump * -2f * Physics.gravity.y);
+        }
+
+
+    }
+}
